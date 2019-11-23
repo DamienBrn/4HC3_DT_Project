1cec01d82379adfc64c4c39f61f6b1ecef4b7325
DamienBrn <damien.berno@outlook.com>
2019-11-23 18:29:26 +0100

    init project

---
+++ src/Main.elm
@@ -0,0 +1,22 @@
+module Main exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+
+
+
+view model =
+    div [ class "jumbotron" ]
+        [ h1 [] [ text "4HC3 FALL 2019" ]
+        , p []
+            [ text "test "
+             
+            ]
+        ]
+
+
+main =
+    view "dummy model"
